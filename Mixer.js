6bdd0e41ab6fc7a2e332be837990acefca134f60
dave <dave41264@hotmail.com>
2022-03-22 23:23:27 +0000

    Add support for convolution  mixing

--- Mixer.js
+++ Mixer.js
@@ -59,7 +59,9 @@ namespace Mixer
 	});
 	
 	pnlMixerControls.setTimerCallback(function()
-	{		
+	{
+		var shouldStop = false;
+		
 		for (i = 0; i < Configuration.mixerGain.length; i++)
 		{
 			var e = Configuration.mixerGain[i];
@@ -67,14 +69,85 @@ namespace Mixer
 			var v = 0.01 * (100.0 + Engine.getDecibelsForGainFactor(gain));
 
 			this.data.levels[i] = v;
+			
+			shouldStop = v < -80;
 		}
 
 		this.repaint();
 	
-		if (!Engine.getNumVoices())
+		if (!Engine.getNumVoices() && shouldStop)
 			this.stopTimer();
 	});
+		
+	// knbPan
+	const knbPan = [];
 	
+	for (i = 0; i < Manifest.channels.length; i++)
+		knbPan.push(Content.getComponent("knbPan" + i));
+	
+	// knbGain
+	const knbGain = [];
+	
+	for (i = 0; i < Manifest.channels.length; i++)
+		knbGain.push(Content.getComponent("knbGain" + i));
+	
+	// btnPurge
+	const btnPurge = []
+	
+	for (i = 0; i < Manifest.channels.length; i++)
+	{
+		btnPurge.push(Content.getComponent("btnPurge" + i));
+		btnPurge[i].setLocalLookAndFeel(LookAndFeel.iconButton);
+	}
+		
+	// cmbOutput
+	const cmbOutput = []
+	
+	for (i = 0; i < Manifest.channels.length; i++)
+	{
+		cmbOutput.push(Content.getComponent("cmbOutput" + i));
+		cmbOutput[i].setLocalLookAndFeel(LookAndFeel.comboBox);
+		cmbOutput[i].setControlCallback(oncmbOutputControl);
+	}	
+
+    inline function oncmbOutputControl(component, value)
+    {
+		local index = cmbOutput.indexOf(component);
+		local matrix = Configuration.masterChain.getRoutingMatrix();
+		local success = true;
+
+		switch(value)
+		{
+			case 1:
+				matrix.addConnection(0 + (index * 2), 0);
+				matrix.addConnection(1 + (index * 2), 1);
+				break;
+
+			case 2:
+				matrix.addConnection(0 + (index * 2), 2);
+				success = matrix.addConnection(1 + (index * 2), 3);
+				break;
+
+			case 3:
+				matrix.addConnection(0 + (index * 2), 4);
+				success = matrix.addConnection(1 + (index * 2), 5);
+				break;
+
+			case 4:
+				matrix.addConnection(0 + (index * 2), 6);
+				success = matrix.addConnection(1 + (index * 2), 7);
+				break;
+		}
+
+		//Reset to Channel 1+2 in case of an error
+		if (!success)
+		{
+			matrix.addConnection(0 + (index * 2), 0);
+			matrix.addConnection(1 + (index * 2), 1);
+		}
+	}
+	
+	// Functions
 	inline function startVuTimer()
 	{
 		pnlMixerControls.startTimer(50);
@@ -83,95 +156,15 @@ namespace Mixer
 	inline function drawVuMeter(g, c, index)
 	{
 		local a = [c.get("x"), c.get("y"), c.getWidth(), c.getHeight()];
-
-		g.setColour(this.get("bgColour"));
-		g.fillRoundedRectangle(a, 8);
-
 		local v = this.data.levels[index];
-
+		
+		g.setColour(this.get("bgColour"));
+		g.fillRoundedRectangle(a, 8);		
+	
 		g.setColour(Colours.withAlpha(this.get("itemColour"), 0.2 * v));
-		g.fillRoundedRectangle([a[0], a[1] + a[3] - a[3] * v, a[2], a[3] * v], 8);
+		g.fillRoundedRectangle([a[0], a[1] + a[3] / 3 + a[3] - a[3] * v, a[2], a[3] * v - a[3] / 3], 8);
 	}
 	
-	// Gather components - knbPan, knbGain, btnPurge, cmbOutput
-	const knbPan = [];
-	const knbGain = [];
-	const btnPurge = [];
-	const cmbOutput = [];
-	
-	for (i = 0; i < Manifest.channels.length; i++)
-	{
-		knbPan.push(Content.getComponent("knbPan" + i));
-		
-		knbGain.push(Content.getComponent("knbGain" + i));
-		
-		btnPurge.push(Content.getComponent("btnPurge" + i));
-		btnPurge[i].setControlCallback(onbtnPurgeControl);
-		
-		cmbOutput.push(Content.getComponent("cmbOutput" + i));
-		cmbOutput[i].setControlCallback(oncmbOutputControl);
-	}
-
-    // btnPurge
-	inline function onbtnPurgeControl(component, value)
-	{
-		local index = btnPurge.indexOf(component);
-		
-		if (isDefined(Configuration.samplers))
-		{
-			for (s in Configuration.samplers)
-			{
-				if (s.asSampler().getNumMicPositions() > 1)
-				{
-					local n = s.asSampler().getMicPositionName(index);
-					s.asSampler().purgeMicPosition(n, !value);
-				}
-			}
-		}
-    }
-
-    // cmbOutput
-    inline function oncmbOutputControl(component, value)
-    {
-		if (isDefined(Configuration.masterChain))
-		{
-			local index = cmbOutput.indexOf(component);
-			local matrix = Configuration.masterChain.getRoutingMatrix();
-			local success = true; // Checks if the output channel exists.
-	
-			switch(value)
-			{
-				case 1:
-					matrix.addConnection(0 + (index * 2), 0);
-					matrix.addConnection(1 + (index * 2), 1);
-					break;
-	                
-				case 2:
-					matrix.addConnection(0 + (index * 2), 2);
-					success = matrix.addConnection(1 + (index * 2), 3);
-					break;
-	                
-				case 3:
-					matrix.addConnection(0 + (index * 2), 4);
-					success = matrix.addConnection(1 + (index * 2), 5);
-					break;
-	                
-				case 4:
-					matrix.addConnection(0 + (index * 2), 6);
-					success = matrix.addConnection(1 + (index * 2), 7);
-					break;
-			}
-	
-			//Reset to Channel 1+2 in case of an error
-			if (!success)
-			{
-				matrix.addConnection(0 + (index * 2), 0);
-				matrix.addConnection(1 + (index * 2), 1);
-			}
-		}
-	}
-	
-	// Functions
 	inline function positionControls()
 	{
 		local channelWidth = vptMixer.getWidth() / Manifest.channels.length;
@@ -190,5 +183,6 @@ namespace Mixer
 		}
 	}
 	
+	// Function calls
 	positionControls();
 }
