eb50fed5d6ce8ae675a86fc95276f58da2fedfb6
dave <dave41264@hotmail.com>
2022-03-29 00:03:35 +0100

    Removed space

--- Expansions.js
+++ Expansions.js
@@ -8,7 +8,7 @@
 
     This file is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
@@ -30,7 +30,7 @@ namespace Expansions
 		if (!value)
 			Expansions.unloadWithPrompt();	
 	}
-	
+
 	// Functions
 	inline function unloadWithPrompt()
 	{
